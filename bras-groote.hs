45229548fd15d2b6a83490d45d4ee807e3190ae5
Dylan Bumford <dbumford@gmail.com>
2012-06-04 13:24:01 -0400

    bras: debugged injective "diff"

--- bras-groote.hs
+++ bras-groote.hs
@@ -1,5 +1,6 @@
 -- mashup of Brasoveanu 2011 with de Groote 2007
 import Debug.Trace
+import Data.List
 
 type Ent = Char
 type Stack = [Ent]
@@ -7,9 +8,9 @@ type Stackset = [Stack]
 type Continuation = Stackset -> Bool
 type Prop = Stackset -> Continuation -> Bool
 
-insert :: Int -> Ent -> Stack -> Stack
-insert 0 x i = x:i
-insert n x (a:i) = a:(insert (n - 1) x i)
+ins :: Int -> Ent -> Stack -> Stack
+ins 0 x i = x:i
+ins n x (a:i) = a:(ins (n - 1) x i)
 
 [a,b,c,d,e,f] = "abcdef"
 domain = [a,b,c,d,e,f]
@@ -40,7 +41,7 @@ conj :: Prop -> Prop -> Prop  -- after de Groote p. 3, (3)
 conj left right is k = left is (\is' -> right is' k)
 
 every n res scope is k = 
-  let fn x y = concat (map (\i -> [insert n x i, insert n y i]) is) in
+  let fn x y = concat (map (\i -> [ins n x i, ins n y i]) is) in
     (and [conj (res n) (scope n) (fn x y) trivial
            | x <- domain, y <- domain, x /= y, res n (fn x y) trivial])
     && (k is)
@@ -49,17 +50,26 @@ every n res scope is k =
 --   such that [i1^{x1/n}, i2^{x2/n}, ..., in^{xm/m}] satisfies res n; scope n
 indef n res scope is k =
   or [conj (res n) (scope n) is' k 
-       | is' <- foldr (\i -> \jss -> [(insert n x i):js | x <- domain, js <- jss]) [[]] is]
+       | is' <- foldr (\i -> \jss -> [(ins n x i):js | x <- domain, js <- jss]) [[]] is]
 
--- different :: Int -> (Int -> Prop) -> Int -> Prop
--- different index nom m = conj (nom m) (\is -> \k -> ((is!!0)!!m) /= ((is!!index)!!m) && (k is))
-
+-- the index on "different" should be 2^n, where n is the number of intervening 
+-- distributors between "different" and its distributor it wants to associate with
 different :: Int -> (Int -> Prop) -> Int -> Prop
-different ind nom m = let
-  neqs is =
-    [((is!!c)!!m) /= ((is!!d)!!m)
-      | c <- [0..length is - 1], d <- [0..length is - 1], ((is!!c)!!ind) /= ((is!!d)!!ind)] in
-  conj (nom m) (\is -> \k -> and (neqs is) && (k is)) 
+different index nom m = conj (nom m) (\is -> \k -> ((is!!0)!!m) /= ((is!!index)!!m) && (k is))
+
+-- butfor is like the g[x]h operation in DPL and CDRT; it checks that stacks
+--   i and j agree on every column except possibly the ones in ns
+butfor :: [Int] -> Stack -> Stack -> Bool
+butfor ns i j = 
+  and [(i!!x) == (j!!x) | x <- filter (\y -> notElem y ns) [0..length i - 1]]
+
+-- "diff" is another implementation option for different. the index on "diff" is
+--   just the index of its associated distributor. it guarantees that if rows 
+--   i and j agree on everything except possibly column n, then they differ on column n
+diff :: Int -> (Int -> Prop) -> Int -> Prop
+diff ind nom m =
+  conj (nom m) (\is -> \k -> and [(i!!m) /= (j!!m)
+        | i <- is, j <- is, i /= j, (butfor [ind,m] i j)] && (k is))
 
 -- eval (john entered) == True
 -- he 0 sat [[a,b]] trivial == True
@@ -87,6 +97,9 @@ give n m l is k = (and (map (\i -> elem ((i!!n), (i!!m), (i!!l))
 give' n m l is k = (and (map (\i -> elem ((i!!n), (i!!m), (i!!l))
                                         [(f,c,a),(e,c,b),(f,d,b),(e,d,a)])
                             is)) && (k is)
+give'' n m l is k = (and (map (\i -> elem ((i!!n), (i!!m), (i!!l))
+                                        [(f,c,a),(e,c,a),(f,d,b),(e,d,b)])
+                            is)) && (k is)
 
 -- Ambiguous: Every boy gave/showed every girl a different poem
 -- eval ((every 0 boy) (\l -> (every 1 girl) (\n -> (indef 2 poem) (\m -> give n m l)))) == True
@@ -95,6 +108,7 @@ give' n m l is k = (and (map (\i -> elem ((i!!n), (i!!m), (i!!l))
 -- eval ((every 0 boy) (\l -> (every 1 girl) (\n -> (indef 2 poem) (\m -> give' n m l)))) == True
 -- eval ((every 0 boy) (\l -> (every 1 girl) (\n -> (indef 2 (different 3 poem)) (\m -> give' n m l)))) == False
 -- eval ((every 0 boy) (\l -> (every 1 girl) (\n -> (indef 2 (different 2 poem)) (\m -> give' n m l)))) == True
+-- eval ((every 0 boy) (\l -> (every 1 girl) (\n -> (indef 2 (different 3 poem)) (\m -> give'' n m l)))) == False
 
 same :: Int -> (Int -> Prop) -> Int -> Prop
 same index nom m = conj (nom m) (\is -> \k -> ((is!!0)!!m) == ((is!!index)!!m) && (k is))
